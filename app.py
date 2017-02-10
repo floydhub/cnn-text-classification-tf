d97772bd47cb964e866e833d8a8b134f9fcd0464
Naren Thiagarajan <narenst@gmail.com>
2017-02-10 11:28:12 -0800

    Add flask app for serving models

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from serve import main, setup
+
+app = Flask(__name__)
+
+
+@app.route("/<input>")
+def evaluate(input):
+    print("Received input: %s".format(input))
+    return str(main([input]))
+
+
+if __name__ == "__main__":
+    setup(checkpoint_dir="./runs/1486683971/checkpoints/")
+    app.run()
